da9df19b9cd7af5ec6c6fafe273c01ac416a6116
Caleb Marcoux <cmeachtech56@gmail.com>
2022-01-22 14:26:09 -0600

    Basic models.

--- Screed/models.py
+++ Screed/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField()
+    email = models.EmailField()
+    score = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Traveler(models.Model):
+    name = models.CharField()
+    user = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    ATTACK = 'A'
+    DEFENSE = 'D'
+    SUPPORT = 'S'
+    TYPES = [
+        (ATTACK, 'Attack'),
+        (DEFENSE, 'Defense'),
+        (SUPPORT, 'Support'),
+    ]
+
+    name = models.CharField()
+    type = models.CharField(
+        max_length=1,
+        choices=TYPES,
+        default=SUPPORT
+    )
+
+class Node(models.Model):
+    title = models.CharField()
+    text = models.TextField()
+    authors = models.ManyToManyField(User)
+
+class Choice(models.Model):
+    text = models.TextField()
+    parent = models.ForeignKey(Node)
+    target = models.ForeignKey(Node)
+
+class Reward(models.Model):
+    choice = models.ForeignKey(Choice)
+    item = models.ForeignKey(Item)
