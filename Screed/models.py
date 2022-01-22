7df5b396d2dd95287e0fe34250348d085174eacd
Uri Herszbaum <uri.herszbaumharding@gmail.com>
2022-01-22 14:35:36 -0600

    Mystery commit

--- Screed/models.py
+++ Screed/models.py
@@ -1,3 +1,8 @@
+from statistics import mode
 from django.db import models
+
+class Screed(models.Model):
+    title= models.CharField(max_length=50),
+    content = models.TextField()
 
 # Create your models here.
--- Screed/views.py
+++ Screed/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+def homepage(request):
+    return render(request, 'base.html', {})
 
 # Create your views here.
