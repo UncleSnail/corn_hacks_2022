7889555e07a3ca03eff71a65178de2b660d03ea9
Caleb Marcoux <cmeachtech56@gmail.com>
2022-01-22 14:33:22 -0600

    Comment models.

--- Screed/models.py
+++ Screed/models.py
@@ -1,47 +1,48 @@
-from django.db import models
+# from django.db import models
 
-class User(models.Model):
-    name = models.CharField()
-    email = models.EmailField()
-    score = models.IntegerField()
+# class User(models.Model):
+#     name = models.CharField()
+#     email = models.EmailField()
+#     score = models.IntegerField()
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
 
-class Traveler(models.Model):
-    name = models.CharField()
-    # user = models.ForeignKey(User)
+# class Traveler(models.Model):
+#     name = models.CharField()
+#     user = models.ForeignKey(User)
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
 
-class Item(models.Model):
-    ATTACK = 'A'
-    DEFENSE = 'D'
-    SUPPORT = 'S'
-    TYPES = [
-        (ATTACK, 'Attack'),
-        (DEFENSE, 'Defense'),
-        (SUPPORT, 'Support'),
-    ]
+# class Item(models.Model):
+#     ATTACK = 'A'
+#     DEFENSE = 'D'
+#     SUPPORT = 'S'
+#     TYPES = [
+#         (ATTACK, 'Attack'),
+#         (DEFENSE, 'Defense'),
+#         (SUPPORT, 'Support'),
+#     ]
 
-    name = models.CharField()
-    type = models.CharField(
-        max_length=1,
-        choices=TYPES,
-        default=SUPPORT
-    )
+#     name = models.CharField()
+#     type = models.CharField(
+#         max_length=1,
+#         choices=TYPES,
+#         default=SUPPORT
+#     )
 
-class Node(models.Model):
-    title = models.CharField()
-    text = models.TextField()
-    authors = models.ManyToManyField(User)
+# class Node(models.Model):
+#     title = models.CharField()
+#     text = models.TextField()
+#     authors = models.ManyToManyField(User)
 
-class Choice(models.Model):
-    text = models.TextField()
-    # parent = models.ForeignKey(Node)
-    # target = models.ForeignKey(Node)
+# class Choice(models.Model):
+#     text = models.TextField()
+#     parent = models.ForeignKey(Node)
+#     target = models.ForeignKey(Node)
 
-class Reward(models.Model):
-    # choice = models.ForeignKey(Choice)
-    # item = models.ForeignKey(Item)
+# class Reward(models.Model):
+#     choice = models.ForeignKey(Choice)
+#     item = models.ForeignKey(Item)
+#     pass
