ab3980fc44393986354d086d067dd3a2b515b83c
gkundwa2-ai <59675047+gkundwa2-ai@users.noreply.github.com>
2022-01-22 17:33:28 -0600

    doing the authentication

--- Screed/views.py
+++ Screed/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from Screed.models import *
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'index.html', {})
+
+@login_required
+def private_place(request):
+    return HttpResponse("Don't have access!", content_type="text/plain")
