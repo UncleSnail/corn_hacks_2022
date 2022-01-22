33e8a2a6a31e9e2ab66092f9d26103a3234e8a50
Caleb Marcoux <cmeachtech56@gmail.com>
2022-01-22 15:24:43 -0600

    Fixed the models.

--- Screed/models.py
+++ Screed/models.py
@@ -1,48 +1,69 @@
-# from django.db import models
+from django.db import models
 
-# class User(models.Model):
-#     name = models.CharField()
-#     email = models.EmailField()
-#     score = models.IntegerField()
+class User(models.Model):
+    name = models.CharField(max_length=128)
+    email = models.EmailField()
+    score = models.IntegerField()
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
 
-# class Traveler(models.Model):
-#     name = models.CharField()
-#     user = models.ForeignKey(User)
+class Traveler(models.Model):
+    name = models.CharField(max_length=128)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True
+    )
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
 
-# class Item(models.Model):
-#     ATTACK = 'A'
-#     DEFENSE = 'D'
-#     SUPPORT = 'S'
-#     TYPES = [
-#         (ATTACK, 'Attack'),
-#         (DEFENSE, 'Defense'),
-#         (SUPPORT, 'Support'),
-#     ]
+class Item(models.Model):
+    ATTACK = 'A'
+    DEFENSE = 'D'
+    SUPPORT = 'S'
+    TYPES = [
+        (ATTACK, 'Attack'),
+        (DEFENSE, 'Defense'),
+        (SUPPORT, 'Support'),
+    ]
 
-#     name = models.CharField()
-#     type = models.CharField(
-#         max_length=1,
-#         choices=TYPES,
-#         default=SUPPORT
-#     )
+    name = models.CharField(max_length=128)
+    type = models.CharField(
+        max_length=1,
+        choices=TYPES,
+        default=SUPPORT,
+    )
 
-# class Node(models.Model):
-#     title = models.CharField()
-#     text = models.TextField()
-#     authors = models.ManyToManyField(User)
+class Node(models.Model):
+    title = models.CharField(max_length=128)
+    text = models.TextField()
+    authors = models.ManyToManyField(User)
 
-# class Choice(models.Model):
-#     text = models.TextField()
-#     parent = models.ForeignKey(Node)
-#     target = models.ForeignKey(Node)
+class Choice(models.Model):
+    text = models.TextField()
+    parent = models.ForeignKey(
+        Node,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='choices'
+    )
+    target = models.OneToOneField(
+        Node,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='reached_by'
+    )
 
-# class Reward(models.Model):
-#     choice = models.ForeignKey(Choice)
-#     item = models.ForeignKey(Item)
-#     pass
+class Reward(models.Model):
+    choice = models.ForeignKey(
+        Choice,
+        on_delete=models.CASCADE
+    )
+    item = models.ForeignKey(
+        Item,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    pass
