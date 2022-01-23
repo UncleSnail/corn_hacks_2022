676ce7daf8387788baa410e1cc46e6728ea47553
Caleb Marcoux <cmeachtech56@gmail.com>
2022-01-22 19:21:42 -0600

    Rework the items.

--- Screed/models.py
+++ Screed/models.py
@@ -19,7 +19,8 @@ class Traveler(models.Model):
     def __str__(self):
         return self.name
 
-class Item(models.Model):
+# The item description does not change. If there is an edit, a new item description is made.
+class ItemDefinition(models.Model):
     ATTACK = 'A'
     DEFENSE = 'D'
     SUPPORT = 'S'
@@ -29,12 +30,34 @@ class Item(models.Model):
         (SUPPORT, 'Support'),
     ]
 
+    # An id for the item that does not change across versions.
+    tag = models.IntegerField()
     name = models.CharField(max_length=256)
     type = models.CharField(
         max_length=1,
         choices=TYPES,
         default=SUPPORT,
     )
+    description = models.TextField()
+    quantity = models.IntegerField(
+        default=0
+    )
+    power = models.IntegerField()
+    cost = models.IntegerField()
+
+class Item(models.Model):
+    item_definition = models.ForeignKey(
+        ItemDefinition,
+        on_delete=models.CASCADE
+    )
+    quantity = models.IntegerField(
+        default=0
+    )
+    owner = models.ForeignKey(
+        Traveler,
+        on_delete=models.CASCADE
+    )
+    
 
 class Node(models.Model):
     title = models.CharField(max_length=512)
@@ -51,6 +74,8 @@ class edit(models.Model):
         on_delete=models.SET_NULL,
         null=True
     )
+    positive = models.IntegerField()
+    negative = models.IntegerField()
 
 class Choice(models.Model):
     text = models.TextField()
@@ -73,8 +98,6 @@ class Reward(models.Model):
         on_delete=models.CASCADE
     )
     item = models.ForeignKey(
-        Item,
-        on_delete=models.SET_NULL,
-        null=True
+        ItemDefinition,
+        on_delete=models.CASCADE
     )
-    pass
