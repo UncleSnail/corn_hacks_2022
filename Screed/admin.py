e54a1fcd3e14c4539620f8c052342483c31ca2b3
Caleb Marcoux <cmeachtech56@gmail.com>
2022-01-22 21:58:10 -0600

    Add checks and stats to the models.

--- Screed/admin.py
+++ Screed/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 
-from .models import User, Traveler, ItemDefinition, Item, Node, Edit, Choice, Reward
+from .models import *
 
 admin.site.register(User)
 admin.site.register(Traveler)
+admin.site.register(StatDefinition)
+admin.site.register(Stat)
 admin.site.register(ItemDefinition)
 admin.site.register(Item)
 admin.site.register(Node)
 admin.site.register(Edit)
+admin.site.register(Check)
 admin.site.register(Choice)
 admin.site.register(Reward)
--- Screed/models.py
+++ Screed/models.py
@@ -8,13 +8,10 @@ class User(models.Model):
     def __str__(self):
         return self.name
 
-class Traveler(models.Model):
-    name = models.CharField(max_length=128)
-    user = models.ForeignKey(
-        User,
-        on_delete=models.SET_NULL,
-        null=True
-    )
+class StatDefinition(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    authors = models.ManyToManyField(User)
 
     def __str__(self):
         return self.name
@@ -24,10 +21,12 @@ class ItemDefinition(models.Model):
     ATTACK = 'A'
     DEFENSE = 'D'
     SUPPORT = 'S'
+    BURDEN = 'B'
     TYPES = [
         (ATTACK, 'Attack'),
         (DEFENSE, 'Defense'),
         (SUPPORT, 'Support'),
+        (BURDEN, 'Burden'),
     ]
 
     # An id for the item that does not change across versions.
@@ -39,14 +38,70 @@ class ItemDefinition(models.Model):
         default=SUPPORT,
     )
     description = models.TextField()
+    authors = models.ManyToManyField(User)
     quantity = models.IntegerField(
         default=0
     )
+    power_type = models.ForeignKey(
+        StatDefinition,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='power_item'
+    )
     power = models.IntegerField(default=0)
+    cost_type = models.ForeignKey(
+        StatDefinition,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='cost_item'
+    )
     cost = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
+    
+
+class Node(models.Model):
+    title = models.CharField(max_length=512)
+    text = models.TextField()
+    authors = models.ManyToManyField(User)
+    positive = models.IntegerField(default=1)
+    negative = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+class Traveler(models.Model):
+    name = models.CharField(max_length=128)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    node = models.ForeignKey(
+        Node,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+    def __str__(self):
+        return self.name
+
+class Stat(models.Model):
+    stat_definition = models.ForeignKey(
+        StatDefinition,
+        on_delete=models.CASCADE,
+        null=True
+    )
+    owner = models.ForeignKey(
+        Traveler,
+        on_delete=models.CASCADE,
+        null=True
+    )
+    score = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.stat_definition} - {self.owner}'
 
 class Item(models.Model):
     item_definition = models.ForeignKey(
@@ -65,16 +120,6 @@ class Item(models.Model):
 
     def __str__(self):
         return f'{self.item_definition} - {self.owner}'
-    
-
-class Node(models.Model):
-    title = models.CharField(max_length=512)
-    text = models.TextField()
-    authors = models.ManyToManyField(User)
-    score = models.IntegerField(default=1)
-
-    def __str__(self):
-        return self.title
 
 class Edit(models.Model):
     title = models.CharField(max_length=512)
@@ -86,7 +131,53 @@ class Edit(models.Model):
         null=True
     )
     positive = models.IntegerField(default=1)
-    negative = models.IntegerField(default=1)
+    negative = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+class Reward(models.Model):
+    node = models.ForeignKey(
+        Node,
+        on_delete=models.CASCADE,
+        null=True
+    )
+    item = models.ForeignKey(
+        ItemDefinition,
+        on_delete=models.CASCADE,
+        null=True
+    )
+
+    def __str__(self):
+        return f'{self.item} <- {self.choice}'
+
+class Check(models.Model):
+    title = models.CharField(max_length=512)
+    success_message = models.TextField()
+    success_target = models.ForeignKey(
+        Node,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='from_success'
+    )
+    failure_message = models.TextField()
+    failure_target = models.ForeignKey(
+        Node,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='from_failure'
+    )
+    stat_type = models.ForeignKey(
+        StatDefinition,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    value = models.IntegerField(default=1)
+    item_requirement = models.ForeignKey(
+        ItemDefinition,
+        on_delete=models.SET_NULL,
+        null=True
+    )
 
     def __str__(self):
         return self.title
@@ -99,27 +190,18 @@ class Choice(models.Model):
         null=True,
         related_name='choices'
     )
-    target = models.OneToOneField(
+    target = models.ForeignKey(
         Node,
         on_delete=models.SET_NULL,
         null=True,
         related_name='reached_by'
     )
+    choice_check = models.OneToOneField(
+        Check,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='parent'
+    )
 
     def __str__(self):
         return self.text
-
-class Reward(models.Model):
-    choice = models.ForeignKey(
-        Choice,
-        on_delete=models.CASCADE,
-        null=True
-    )
-    item = models.ForeignKey(
-        ItemDefinition,
-        on_delete=models.CASCADE,
-        null=True
-    )
-
-    def __str__(self):
-        return f'{self.item} <- {self.choice}'
