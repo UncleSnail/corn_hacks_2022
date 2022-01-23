4a2963ff39767e279e6a1ed7d7cb12e825c7ed98
gkundwa2-ai <59675047+gkundwa2-ai@users.noreply.github.com>
2022-01-22 20:25:20 -0600

    changes made before doing the authentication

--- Screed/views.py
+++ Screed/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from Screed.models import *
+from django.contrib.auth.forms import UserCreationForm
+
 
 
 def index(request):
     return render(request, 'index.html', {})
 
-@login_required
-def private_place(request):
-    return HttpResponse("Don't have access!", content_type="text/plain")
+def register(request):
+    register_form = UserCreationForm()
+    return render(request, 'register.html', {'register_form': register_form})
--- corn_hacks_2022/urls.py
+++ corn_hacks_2022/urls.py
@@ -1,12 +1,14 @@
 
 from django.contrib import admin
-from django.urls import include, path
-from Screed import views
+from django.urls import path, include
+from Screed import views as Screed_views
 
 
 urlpatterns = [
     path('Screed/', include('Screed.urls')),
     path('admin/', admin.site.urls),
-    path('private_place/', views.private_place),
-    path('accounts/', include("django.contrib.auth.urls")),
+    # path('private_place/', views.private_place),
+    # path('accounts/', include("django.contrib.auth.urls")),
+    # path('add_messages/',views.add_messages),
+
 ]
